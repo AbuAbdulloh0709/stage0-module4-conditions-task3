478388a12420f4c09b221ed32ca72320342422b1
Muslimbek Soliyev <abuabdulloh0709@gmail.com>
2023-01-10 14:55:04 +0500

    module 4 task 3

--- src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
+++ src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
@@ -2,6 +2,20 @@ package school.mjc.stage0.conditions.task3;
 
 public class Seasons {
     public void tellTheSeasonByMonthNumber(int month) {
-
+        if (month < 1 || month > 12) {
+            System.out.println("wrong number!");
+            return;
+        }
+        if ((month) / 3 == 0) {
+            System.out.println("Winter");
+        } else if ((month) / 3 == 1) {
+            System.out.println("Spring");
+        } else {
+            if ((month) / 3 == 2) {
+                System.out.println("Summer");
+            } else if ((month) / 3 == 3) {
+                System.out.println("Autumn");
+            }
+        }
     }
 }
